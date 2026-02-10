cbe6f91177a3281ebfc2403757a710f3df02e477
aaronw176 <aaronw176@nycstudents.net>
2026-02-10 09:12:16 -0500

    cookies

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-item = {
-    "name": "cookies"
 
+item_bakery = {
+    "name": "very good cookies", "price": "1.50", "description": "definetly perfect cookies",
 
+    "name": "fanastic cake", "price": "50", "description": "this will end better than cookies",
 
+    "name": "cayley", "price": "free", "description": "she will make very good cookies"
 
 }
+item = input("What would you like? ")
+
+if item == "cookies":
+    print(item_bakery[0] ["name"] )
+elif item == "cake":
+    print(item_bakery[1] ["name"] )
+elif item == "devil":
+    print(item_bakery[] ["name"] )
+else:
+    print("We don't have that here")
