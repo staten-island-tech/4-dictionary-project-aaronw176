f7ff6eee03dae290bfb5f77492c75dc54bd57f57
aaronw176 <aaronw176@nycstudents.net>
2026-02-25 09:14:28 -0500

    bakery

--- app.py
+++ app.py
@@ -5,22 +5,25 @@ item_bakery = [
     "price": float("7.50"), 
     "description": "definetly perfect cookies"
     },
+
     {"name": "fanastic cake", 
     "price": int("50"), 
-    "description": "this will end better than cookies"},
+    "description": "this will end better than cookies"
+    },
 
     {"name": "pboy", 
     "price": int("0"), 
     "description": "he will make very good cookies"
-}
-{
+    },
+
+    {
     "name": "cayley",
-    "price": int("1")
+    "price": int("1"),
     "description": "she is a cereal killer you might not want this..."
     }
-]
-receipt = [
-]
+    ]
+receipt = []
+
 price = []
 
 for index, item in enumerate(item_bakery):
@@ -28,42 +31,38 @@ for index, item in enumerate(item_bakery):
 
 item = int(input("What would you like? "))
 
-if item == 0 :
-    print(item_bakery [0]["name"])
-    receipt.append(item_bakery [0]["name"])
-    price.append(item_bakery [0]["price"])
-elif item == 1 :
-    print(item_bakery [1]["name"] )
-    receipt.append(item_bakery [1]["name"])
-    price.append(item_bakery [1]["price"])
-elif item == 2 :
-    print(item_bakery [2] ["name"])
-    receipt.append(item_bakery [2]["name"])
-    price.append(item_bakery [2]["price"])
+if isinstance(item, int):
+    print(item_bakery[item]["name"])
+    price.append(item_bakery [item]["price"])
+    receipt.append(item_bakery [item]["name"])
+
 else:
     print("We don't have that here")
-
+    
 more = input("Would you like to continue purchasing? ")
+
 while more == "yes":
+
     item = int(input("What would else would you like? "))
-    if item == 0 :
-        print(item_bakery [0]["name"])
-        receipt.append(item_bakery [0]["name"])
-        price.append(item_bakery [0]["price"])
-    elif item == 1 :
-        print(item_bakery [1]["name"] )
-        receipt.append(item_bakery [1]["name"])
-        price.append(item_bakery [1]["price"])
-    elif item == 2 :
-        print(item_bakery [2] ["name"])
-        receipt.append(item_bakery [2]["name"])
-        price.append(item_bakery [2]["price"])
-    else:
+
+    if isinstance(item, int):
+
+        print(item_bakery[item]["name"])
+        price.append(item_bakery [item]["price"])
+        receipt.append(item_bakery [item]["name"])
+        more = input("Would you like to continue purchasing? ")
+
+    elif more == "no":
+        break
+
+    else: 
+
         print("We don't have that here")
-    more = input("Would you like to continue purchasing? ")
+        more = input("Would you like to continue purchasing? ")
+
 exit = input("Thank you for purchasing our items. Would you like an receipt ")
+
 if exit == "yes": 
     for index, item in enumerate(receipt):
         print(index, ":", item)
     print(f"Your total is ${sum(price)}")
-        
